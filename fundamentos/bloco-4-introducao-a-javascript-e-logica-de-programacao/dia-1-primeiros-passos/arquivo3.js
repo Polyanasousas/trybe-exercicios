ecaa62c2c99b5015ab65d5342b96c5996f92baa6
Polyanasousas <souzaspolyana@gmail.com>
2021-11-12 02:23:45 -0300

    cria novo arquivo javascript

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-primeiros-passos/arquivo3.js
@@ -0,0 +1,11 @@
+/* Exercicios de fixação: Condições If e Else */
+
+const nota = 75;
+
+if (nota >= 80) {
+    console.log("Parabéns, você foi aprovado")
+} else if (nota >= 60 && nota < 80) {
+    console.log("Você esta na lista de espera")
+} else {
+    console.log("Você foi reprovado")
+};
