df64ad797d0a2030012609f3ef155e0f8a17c1bd
Polyanasousas <souzaspolyana@gmail.com>
2021-11-23 00:55:06 -0300

    exercicio 1

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -14,3 +14,17 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+  // exercicio 1
+  function createDaysOfTheMonth() {
+    const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    const MonthDaysList = document.querySelector('#days');
+
+    for (let i = 0; i < dezDaysList.length; i += 1) {
+        const MonthDays = dezDaysList[i];
+        const MonthDayListItem = document.createElement('li');
+        MonthDayListItem.innerHTML = MonthDays;
+    
+        MonthDaysList.appendChild(MonthDayListItem);
+      };
+  }
+  createDaysOfTheMonth();
